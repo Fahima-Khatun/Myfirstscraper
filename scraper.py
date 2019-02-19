89bdebbf01bf72283fa2d4fe9701afff09c36cc8
Fahima-Khatun <43742381+Fahima-Khatun@users.noreply.github.com>
2019-02-19 12:16:59 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,6 +16,7 @@ print(root.cssselect("div#footer"))
 listofmatches=root.cssselect("a")
 for match in listofmatches:
   print(match)
+  print(lxml.html.tostring/variablename)
 ##<a lookig for te liks
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
